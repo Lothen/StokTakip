47766ad31e00c669cce452405f80a7f9b1dc8c82
Gokhan <ozdemirgokhan11@gmail.com>
2026-01-22 02:25:10 +0300

    Supabase dosyasi tasiyarak isim duzeltildi v5

--- src/lib/SupabaseClient.js
+++ src/lib/SupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Supabase panelinden Project Settings -> API kısmından bu bilgileri alıp tırnak içlerine yapıştırın  frmemefdader
+// Supabase panelinden Project Settings -> API kısmından bu bilgileri alıp tırnak içlerine yapıştırın  
 const supabaseUrl = 'https://kepayowtyrnphepvsvsr.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlcGF5b3d0eXJucGhlcHZzdnNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5MDA2NDYsImV4cCI6MjA4MzQ3NjY0Nn0.eqYzxCXwQqdeILX8dNcpwHgLpcOryLYFm4I4ARghNnM'
 
